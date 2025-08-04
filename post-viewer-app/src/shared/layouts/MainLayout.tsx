ed26d80476d8769296c4ed51cfcabda9351fe242
Diana Grayden <graydendi@mail.ru>
2025-08-04 16:48:21 +0300

    feat: add filesand code for create header and footer

--- post-viewer-app/src/shared/layouts/MainLayout.tsx
+++ post-viewer-app/src/shared/layouts/MainLayout.tsx
@@ -1,7 +1,12 @@
+import Header from "../../widgets/LayoutHeader/Header";
+import Footer from "../../widgets/LayoutFooter/Footer";
+
 const MainLayout = () => {
   return (
     <div>
+      <Header />
       <h1>Hello World</h1>
+      <Footer />
     </div>
   )
 }
---
+++ post-viewer-app/src/widgets/LayoutHeader/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header>Header for Post Viewer</header>;
+};
+
+export default Header
