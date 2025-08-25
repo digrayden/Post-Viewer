ece7709269e34ed463f37137089094624aa0f073
Diana Grayden <graydendi@mail.ru>
2025-08-25 16:29:48 +0300

    fix: change location of types files

--- post-viewer-app/src/entities/album/api/albumsApi.ts
+++ post-viewer-app/src/entities/album/api/albumsApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { Album } from '../types';
+import type { Album } from '../model/types';
 
 const API_URL = 'https://jsonplaceholder.typicode.com';
 
--- post-viewer-app/src/entities/comment/api/commentsApi.ts
+++ post-viewer-app/src/entities/comment/api/commentsApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { Comment } from '../types';
+import type { Comment } from '../model/types';
 
 const API_URL = 'https://jsonplaceholder.typicode.com';
 
