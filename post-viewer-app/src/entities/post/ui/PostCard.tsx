64290b38e694a4004daec99029970c64975e1a65
Diana Grayden <graydendi@mail.ru>
2025-08-26 16:42:25 +0300

    fix: change postcard

--- post-viewer-app/src/entities/post/ui/PostCard.tsx
+++ post-viewer-app/src/entities/post/ui/PostCard.tsx
@@ -1,6 +1,7 @@
 import type { Post } from "../model/types";
 import styles from './PostCard.module.css';
 import { useNavigate } from 'react-router-dom';
+import type { MouseEventHandler } from 'react';
 
 interface PostCardProp {
   post: Post;
@@ -10,19 +11,21 @@ interface PostCardProp {
 const PostCard = ({ post, clickable = true }: PostCardProp) => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleClick: MouseEventHandler<HTMLDivElement> = () => {
     if (clickable) {
       navigate(`/posts/${post.id}`);
     }
   };
   
   return (
-    <article className={`${styles.card} ${clickable ? styles.clickable : ''}`}
-    onClick={handleClick}>
+    <article 
+      className={`${styles.card} ${clickable ? styles.clickable : ''}`}
+      onClick={handleClick}
+    >
       <h3 className={styles.title}>{post.title}</h3>
       <p className={styles.body}>{post.body}</p>
     </article>
   );
 };
 
-export default PostCard
+export default PostCard;
